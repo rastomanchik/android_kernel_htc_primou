bcd06be1326e7c72b09dc41c8fe53b84cef6ac9f
Aleksandr <rastomanchik77@gmail.com>
2014-08-13 11:22:26 +0300

    rebuild

---
+++ arch/arm/mach-msm/include/mach/debug_display.h
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+#ifndef __LOG_MESSAGE_H__
+#define __LOG_MESSAGE_H__
+
+#define PR_DISP_ERR(fmt, args...) 		pr_err("[DISP] "fmt, ##args);
+#define PR_DISP_WARN(fmt, args...) 		pr_warn("[DISP] "fmt, ##args);
+#define PR_DISP_INFO(fmt, args...) 		pr_info("[DISP] "fmt, ##args)
+#define PR_DISP_DEBUG(fmt, args...) 		pr_debug("[DISP] "fmt, ##args);
+
+#endif /* __LOG_MESSAGE_H__ */
